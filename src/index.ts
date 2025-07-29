f04169e3ff347e792f76d970ce46f352250253ea
Rahul <rahul.kumar@vegavid.com>
2025-07-30 03:00:46 +0530

    express port added

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import "dotenv/config";
+import express from "express";
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running on port 3000");
+});
 // import connectToDb from './models/connection';
 import { initializeBot } from "./services/bot";
 function init() {
