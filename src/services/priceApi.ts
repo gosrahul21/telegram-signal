7f4b787cbc6b5bcd772c948b882a0529f93a50ee
Rahul <rahul.kumar@vegavid.com>
2025-09-06 21:08:30 +0530

    change

--- src/services/priceApi.ts
+++ src/services/priceApi.ts
@@ -1,24 +1,4 @@
-import { Duration } from "../types/Duration";
-import axios from "axios";
-
-// // Function to fetch candle data from the Coindcx API
-// export async function fetchCandleData(pair: string, interval: Duration) {
-//     try {
-//         const url = `https://public.coindcx.com/market_data/candles/?pair=${pair}&interval=${interval}`;
-//         const response: any = await axios.get(url);
-//         const data = await response.data;
-//         return data;
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-// export async function fetchTickerPrice() {
-//     const url = `https://api.coindcx.com/exchange/ticker/`;
-//     const response: any = await axios.get(url);
-//     const data = await response.data;
-//     return data;
-// }
+import axios from "axios";  
 
 // Function to fetch candlestick (OHLC) data from Binance API
 export async function fetchCandleData(symbol: string, interval: string) {
