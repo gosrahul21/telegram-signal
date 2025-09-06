9e0690c5da1224d1fad27747cf268d44c956989e
gosrahul21 <gosrahul21@gmail.com>
2025-09-06 14:00:22 +0530

    notification payload fix

--- src/modules/notification/notification.service.ts
+++ src/modules/notification/notification.service.ts
@@ -46,7 +46,7 @@ export class NotificationService {
 
     // Emit notification created event
 
-    this.emitNotification({ ...savedNotification.toObject() });
+    this.emitNotification({ ...savedNotification.toObject() } as NotificationCreatedPayload);
     return savedNotification;
   }
 
@@ -407,7 +407,7 @@ export class NotificationService {
 
       // Emit events for each created notification
       result.forEach((notification) => {
-        this.emitNotification({ ...notification.toObject() });
+        this.emitNotification({ ...notification.toObject() } as NotificationCreatedPayload);
       });
 
       this.logger.log(`Created ${createdCount} bulk notifications`);
